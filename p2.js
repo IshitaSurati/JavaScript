45c9dfd7ca4a19bdfb59cd80e603771d00c07f06
Ishitasurat25 <ishitasurati83@gmail.com>
2024-04-16 17:34:07 +0530

    complete

---
+++ p2.js
@@ -0,0 +1,13 @@
+/*
+Create a variable name to store your name
+Create a variable age to store your age
+Print the values stored in the variable on one line followed by the type of the variable in the next line
+ */
+
+// Create variables to store name and age
+let name = "Ishita";
+let age = 20;
+
+// Print the values stored in the variables along with their types
+console.log(name, age);
+console.log(typeof name, typeof age);
