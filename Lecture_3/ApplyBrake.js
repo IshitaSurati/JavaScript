417027ad248d05d03a7c032cdd8401dc7e218030
Ishitasurat25 <ishitasurati83@gmail.com>
2024-04-18 22:39:10 +0530

    lec3

---
+++ Lecture_3/ApplyBrake.js
@@ -0,0 +1,9 @@
+let distance=100, time=2;
+console.log("Input\n",distance, time);
+let speed = distance/time
+console.log("Output");
+if (speed > 40) {
+    console.log("Apply Brake");
+} else {
+   console.log("Keep Going"); 
+}
