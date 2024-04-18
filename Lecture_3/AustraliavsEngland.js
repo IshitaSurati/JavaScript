e811fcc76d65fe68a12c38d98c603387deafbc22
Ishitasurat25 <ishitasurati83@gmail.com>
2024-04-18 22:33:14 +0530

    lec3

---
+++ Lecture_3/AustraliavsEngland.js
@@ -0,0 +1,13 @@
+let Australia=46, England= 67;
+console.log("Input\n",Australia, England);
+
+console.log("Output");
+if (Australia>England) {
+  console.log("Australia");
+} 
+else if (Australia<England) {
+  console.log("England");
+} 
+else if (Australia==England) {
+    console.log("Equal");
+} 
