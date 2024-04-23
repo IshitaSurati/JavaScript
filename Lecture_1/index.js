0fb92446269deca87337b6318d22403b714d9ef3
Ishitasurat25 <ishitasurati83@gmail.com>
2024-04-23 21:14:29 +0530

    Lec 1

---
+++ Lecture_1/index.js
@@ -0,0 +1,16 @@
+//introduction 
+console.log("Introduction of JavaScript :\n");
+//generally used let 
+let a=10;
+let b=20;
+console.log(a+b);
+ 
+//const
+const c=20
+// c=40 do not reassign the value
+console.log(c);
+
+//old version
+var age=20
+var name="ishita"
+console.log(age,name);
