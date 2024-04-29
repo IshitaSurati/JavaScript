6cde675641b76fb3d7e531fa826d3614b454624d
Ishitasurat25 <ishitasurati83@gmail.com>
2024-04-29 22:24:35 +0530

    update

---
+++ Lecture_11/usepopandtoString.js
@@ -0,0 +1,4 @@
+let array = [1, 2, 3, 4, 5, 6]
+console.log("Before Pop : ",array);
+let last = array.pop()
+console.log("Aftere Pop : ",array);
---
+++ Practice.js
@@ -0,0 +1,77 @@
+// push
+// let numbers = [1, 2, 3];
+// console.log(numbers);
+// numbers.push(4, 5);
+// console.log(numbers);
+// // Result: numbers = [1, 2, 3, 4, 5]
+
+// pop
+// let fruits = ['apple', 'banana', 'orange'];
+// console.log(fruits);
+// let lastFruit = fruits.pop();
+// // lastFruit is 'orange', fruits is now ['apple', 'banana']
+// console.log(fruits);
+
+// length
+// let numbers = [1, 2, 3, 4, 5];
+// console.log(numbers.length);
+// // Result: 5
+
+
+// // toString
+// let temperatures = [25, 28, 30, 29, 27, 26, 24];
+// console.log(temperatures);
+// let temperatureString = temperatures.toString();
+// console.log(temperatureString);
+// // Result: fruitString = 25, 28, 30, 29, 27, 26, 24
+
+// // splice
+// let fruits = ['apple', 'banana', 'orange'];
+// console.log(fruits);
+// fruits.splice(1, 2, 'grape', 'melon');
+// console.log(fruits);
+// // Result: fruits = ['apple', 'grape', 'melon', 'orange']
+
+// slice
+// let fruits = ['apple', 'banana', 'orange', 'grape'];
+// console.log(fruits);
+// let slicedFruits = fruits.slice(1, 3);
+// console.log(slicedFruits);
+// // Result: slicedFruits = ['banana', 'orange'], fruits = ['apple', 'banana', 'orange', 'grape']
+
+// sort
+// let fruits = ['banana', 'apple', 'orange', 'grape'];
+// console.log(fruits);
+// fruits.sort();
+// console.log(fruits);
+// // Result: fruits = ['apple', 'banana', 'grape', 'orange']
+
+// shift
+// let A = [1, 2, 3];
+// console.log(A);
+// let shiftedA = A.shift();
+// console.log(shiftedA);
+// console.log(A);
+// // Result: shiftedFruit = 'apple', fruits = ['banana', 'orange']
+
+
+// unshift
+// let fruits = ['banana', 'orange'];
+// let newLength = fruits.unshift('apple', 'grape');
+// console.log(newLength);
+// console.log(fruits);
+// // Result: newLength = 4, fruits = ['apple', 'grape', 'banana', 'orange']
+
+// concat
+// let fruits = ['a', 'b'];
+// console.log(fruits);
+// let moreFruits = fruits.concat(['c', 'd']);
+// console.log(moreFruits);
+// // Result: moreFruits = ['apple', 'banana', 'orange', 'grape']
+
+// reverse
+let fruits = ['apple', 'banana', 'orange', 'grape'];
+console.log(fruits); 
+fruits.reverse();
+console.log(fruits);
+// Result: fruits = ['grape', 'orange', 'banana', 'apple']
