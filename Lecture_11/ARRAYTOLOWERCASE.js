22e37bf6cd19ff099ca27bb97bbff0d9a757e2fd
Ishitasurat25 <ishitasurati83@gmail.com>
2024-05-01 18:14:15 +0530

    update

--- Lecture_11/ARRAYTOLOWERCASE.js
+++ Lecture_11/ARRAYTOLOWERCASE.js
@@ -4,4 +4,19 @@ const print = (array) => {
         console.log(array[i]);
     }
 }
+console.log("Befor:\n");
 print(array)
+
+const upp=(array)=>{
+    for (let i = 0; i < array.length; i++) {
+        if (i%2==0) {
+            console.log(array[i]);
+        } else {
+            console.log(array[i].toLowerCase());
+        }
+        
+    }
+}
+
+console.log("\nAfter:\n");
+upp(array)
---
+++ Lecture_13/PrintCharactersofaString.js
@@ -0,0 +1,7 @@
+let str="rednwhite"
+console.log("Before : ",str)
+console.log("After : ")
+for (let i = 0; i < str.length; i++) {
+    const element = str[i];
+    console.log(str[i]);   
+}
