24b8a7bc9da5311b958f374584b08f41d3d67739
Ishitasurat25 <ishitasurati83@gmail.com>
2024-05-13 21:37:11 +0530

    update

---
+++ Lecture_13/CheckVowel.js
@@ -0,0 +1,12 @@
+let count=0
+let str="red"
+console.log(str);
+for (let i = 0; i < str.length; i++) {
+    if (count>0) {
+        console.log(true);  
+    }
+    else
+    {
+        console.log(false);
+    }
+}
